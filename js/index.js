e57166d7945a68624adb5e80b5d11b6f6fb7bdbe
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-02-29 20:16:35 -0700

    created refVars. began aels and functions

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-console.log("js running");
+const enterAlbumTitle = document.querySelector("#album-title");
+const enterAlbumDescription = document.querySelector("#album-description");
+const chooseAlbumArt = document.querySelector("#album-art");
+const saveButton = document.querySelector("submit-btn");
+
+
+saveButton.addEventListener("submit", onSaveAlbum);
+
+function onSaveAlbum(e) {
+    e.preventDefault();
+    
+}
+
+
+
+
+
