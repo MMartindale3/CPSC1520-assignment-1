bc9c41bbf8898f98d5bf06a170bae16f412acaa4
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-03-03 17:51:31 -0700

    basic functions

--- js/index.js
+++ js/index.js
@@ -1,17 +1,33 @@
-const enterAlbumTitle = document.querySelector("#album-title");
-const enterAlbumDescription = document.querySelector("#album-description");
-const chooseAlbumArt = document.querySelector("#album-art");
-const saveButton = document.querySelector("submit-btn");
+const saveAlbumForm = document.querySelector("#album-form");
+const albumTitle = document.querySelector("#album-title");
+const albumDescription = document.querySelector("#album-description");
+const albumArt = document.querySelector("#album-art");
 
 
-saveButton.addEventListener("submit", onSaveAlbum);
-
+// test if input of title and desc are empty on submit
+saveAlbumForm.addEventListener("submit", onSaveAlbum);
 function onSaveAlbum(e) {
     e.preventDefault();
+    console.log(e.currentTarget.elements["album-title"].value.length);    
+}
+
+// test if title is >10 char during input
+albumTitle.addEventListener("input", onTypeTitle);  
+function onTypeTitle(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.value.length);
     
 }
 
+albumDescription.addEventListener("input", onTypeDescription);
+function onTypeDescription(e) {
+    e.preventDefault();
+    console.log(e.currentTarget.value.length);
+}
 
-
-
+albumArt.addEventListener("change", onSelectArt);
+function onSelectArt(e) {
+    e.preventDefault();
+    
+}
 
