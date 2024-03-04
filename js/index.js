d1616e60a03ba28f0d41d2e0367f6b0eb68c0841
MMartindale3 <145407645+MMartindale3@users.noreplay.github.com>
2024-03-03 22:25:38 -0700

    Completed validation steps

--- js/index.js
+++ js/index.js
@@ -6,28 +6,81 @@ const albumArt = document.querySelector("#album-art");
 
 // test if input of title and desc are empty on submit
 saveAlbumForm.addEventListener("submit", onSaveAlbum);
+
 function onSaveAlbum(e) {
     e.preventDefault();
-    console.log(e.currentTarget.elements["album-title"].value.length);    
+    hasTitle = albumTitle.value.trim();
+    hasDescription = albumDescription.value.trim();
+    goodAlbum = true;
+
+    if (hasTitle === "") {
+        albumTitle.classList.add("is-invalid");
+        goodAlbum = false;
+    } if (hasTitle !== "") {
+        albumTitle.classList.remove("is-invalid");
+    }
+
+    if (hasDescription === "") {
+        albumDescription.classList.add("is-invalid");
+        goodAlbum = false;
+    } if (hasDescription !== "") {
+        albumDescription.classList.remove("is-invalid");        
+    }
+
+    if (albumArt.value === "") {
+        albumArt.classList.add("is-invalid");
+        goodAlbum = false;
+    }
+    if (albumArt.value !== "") {
+        albumArt.classList.remove("is-invalid");
+    }
+
+    if (goodAlbum == true) {
+        console.log("you can save this album!");
+        
+    }
+    if (goodAlbum != true) {
+        console.log("Please enter correct album inputs.");
+        
+    }
 }
 
-// test if title is >10 char during input
-albumTitle.addEventListener("input", onTypeTitle);  
-function onTypeTitle(e) {
-    e.preventDefault();
-    console.log(e.currentTarget.value.length);
-    
-}
+// test if title >15 char during input
+albumTitle.addEventListener("input", onHandleTitle);
 
-albumDescription.addEventListener("input", onTypeDescription);
-function onTypeDescription(e) {
+function onHandleTitle(e) {
+    e.preventDefault();    
+    const characterCount = e.currentTarget.value.length;
+    if (characterCount > 15) {
+        albumTitle.classList.add("is-invalid");
+    }
+    if (characterCount <= 15) {
+        albumTitle.classList.remove("is-invalid");
+    }
+}
+// if description >30
+albumDescription.addEventListener("input", onHandleDescription);
+
+function onHandleDescription(e) {
     e.preventDefault();
-    console.log(e.currentTarget.value.length);
+    const characterCount = e.currentTarget.value.length;
+    if (characterCount > 30) {
+        albumDescription.classList.add("is-invalid");
+    }
+    if (characterCount <= 30) {
+        albumDescription.classList.remove("is-invalid");
+    }
 }
 
 albumArt.addEventListener("change", onSelectArt);
 function onSelectArt(e) {
     e.preventDefault();
-    
+    if (albumArt.value === "") {
+        albumArt.classList.add("is-invalid");
+    }
+    if (albumArt.value !== "") {
+        albumArt.classList.remove("is-invalid");
+    }
+
 }
 
